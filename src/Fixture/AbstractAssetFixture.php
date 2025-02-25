38f885c476bf2bc968a945896cca4b07060c919e
Luka Dschaak <l.dschaak@neusta.de>
2025-02-25 22:44:50 +0100

    [asset-fixture] use static calls for now to keep it simple and stupid

--- src/Fixture/AbstractAssetFixture.php
+++ src/Fixture/AbstractAssetFixture.php
@@ -2,13 +2,12 @@
 
 namespace Neusta\Pimcore\FixtureBundle\Fixture;
 
-use Neusta\Pimcore\FixtureBundle\Toolbox\Repository\AssetRepository;
+use Pimcore\Model\Asset;
 use Pimcore\Model\Asset\Image;
 
 abstract class AbstractAssetFixture extends AbstractFixture
 {
     public function __construct(
-        protected AssetRepository $assetRepository,
         protected string $fullQualifiedFilename,
         protected string $pimcoreFilename,
         protected string $pimcoreBasePath,
@@ -21,7 +20,7 @@ abstract class AbstractAssetFixture extends AbstractFixture
         $asset = new Image();
         $asset->setFilename($this->pimcoreFilename);
         $asset->setData(file_get_contents($this->fullQualifiedFilename));
-        $asset->setParent($this->assetRepository->getByPath($this->pimcoreBasePath));
+        $asset->setParent(Asset::getByPath($this->pimcoreBasePath));
 
         $this->replaceIfExists($asset);
 
@@ -30,7 +29,7 @@ abstract class AbstractAssetFixture extends AbstractFixture
 
     private function replaceIfExists(Image $asset): void
     {
-        $oldAsset = $this->assetRepository->getByPath($this->pimcoreBasePath . $asset->getFullPath());
+        $oldAsset = Asset::getByPath($this->pimcoreBasePath . $asset->getFullPath());
         if (null !== $oldAsset) {
             $oldAsset->delete();
         }
