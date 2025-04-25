7ffaa6a6873218fdba992d3673d9e8582ffa801d
Jacob Dreesen <j.dreesen@neusta.de>
2025-04-26 00:10:14 +0200

    Refactor FixtureDependencySorter for improved efficiency (#33)

--- src/EventListener/SortFixturesByDependencyBeforeLoad.php
+++ src/EventListener/SortFixturesByDependencyBeforeLoad.php
@@ -3,17 +3,13 @@
 namespace Neusta\Pimcore\FixtureBundle\EventListener;
 
 use Neusta\Pimcore\FixtureBundle\Event\BeforeLoadFixtures;
-use Neusta\Pimcore\FixtureBundle\Fixture\Fixture;
 use Neusta\Pimcore\FixtureBundle\Sorter\FixtureDependencySorter;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 final class SortFixturesByDependencyBeforeLoad implements EventSubscriberInterface
 {
-    /**
-     * @param \Traversable<Fixture> $allFixtures
-     */
     public function __construct(
-        private readonly \Traversable $allFixtures,
+        private readonly FixtureDependencySorter $dependencySorter,
     ) {
     }
 
@@ -26,7 +22,6 @@ final class SortFixturesByDependencyBeforeLoad implements EventSubscriberInterfa
 
     public function sortFixturesByDependency(BeforeLoadFixtures $event): void
     {
-        $event->fixtures = (new FixtureDependencySorter(iterator_to_array($this->allFixtures, false)))
-            ->sort($event->fixtures);
+        $event->fixtures = $this->dependencySorter->sort($event->fixtures);
     }
 }
